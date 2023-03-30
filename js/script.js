4330c4d871cee64b23cade601f8702848768df9d
Samuel Colares <samuelcolaresdeaquino@gmail.com>
2023-03-30 00:13:24 -0300

    Organização do javascript

--- js/script.js
+++ js/script.js
@@ -1,117 +1,106 @@
-const smallcups = document.querySelectorAll('.small-cups')
-const bigwater = document.querySelector('.big-water')
-const mls = document.querySelectorAll('.ml')
-const meta = document.querySelector('.metaDiaria')
-const restante = document.querySelector(`.restante`)
 
+const Main = {
+    init: function () {
+        this.cacheSelectors()
+        this.bindEvents()
+    },
+    cacheSelectors: function () {
+        this.smallcups = document.querySelectorAll('.small-cups')
+        this.bigwater = document.querySelector('.big-water')
+        this.mls = document.querySelectorAll('.ml')
+        this.meta = document.querySelector('.metaDiaria')
+        this.restante = document.querySelector(`.restante`)
+        this.range = document.getElementById('range')
+        this.rangeV = document.getElementById('rangeV')
+    },
+    bindEvents: function () {
+        document.addEventListener("DOMContentLoaded", this.Events.setValue.bind(this));
+        this.smallcups.forEach((cup, idx) => {
+            const handleClick = this.Events.copoCheio.bind(this, idx);
+            cup.addEventListener(`click`, handleClick);
+            cup.addEventListener('click', this.Events.copao.bind(this))
+          });
 
-smallcups.forEach((cup, idx) => {
-    cup.addEventListener('click', () => {
-        copoCheio(idx)
-        copao()
-    })
+        this.range.addEventListener('input', this.Events.setValue.bind(this));
+    },
+    updateCounter: function (numeroNovo) {
+        let n1 = parseInt(this.bigwater.nextElementSibling.innerText)
+        let n2 = +numeroNovo
+        const intervalo = setInterval(() => {
 
-})
+            if (n1 <= n2) {
+                this.bigwater.nextElementSibling.innerText = `${n1++}%`
+            } else {
+                clearInterval(intervalo)
+            }
+        }, 20)
+    },
+    updateCounterMenos: function (numeroNovo) {
+        let n4 = parseInt(this.bigwater.nextElementSibling.innerText)
+        let n3 = +numeroNovo
+        console.log(n3, n4)
+        const intervalo = setInterval(() => {
 
-function copoCheio(idx) {
-    if (smallcups[idx].classList.contains(`filled`) && !smallcups[idx + 1].classList.contains(`filled`)) {
-        idx--
-        bigwater.style.height = `${(idx + 1) * 9.7}%`
-        updateCounterMenos(((idx + 1) * 10))
+            if (n4 >= n3) {
+                this.bigwater.nextElementSibling.innerText = `${n4--}%`
+            } else {
+                clearInterval(intervalo)
+            }
+        }, 20)
+    },
+    Events: {
+        copoCheio: function (idx) {
+            if (this.smallcups[idx].classList.contains(`filled`) && !this.smallcups[idx + 1].classList.contains(`filled`)) {
+                idx--
+                this.bigwater.style.height = `${(idx + 1) * 9.7}%`
+                this.updateCounterMenos(((idx + 1) * 10))
+            }
+            this.smallcups.forEach((cup, idx2) => {
+                if (idx2 <= idx) {
+                    cup.classList.add(`filled`)
+                    this.bigwater.style.height = `${(idx + 1) * 9.95}%`
+                    this.mls[idx2].style.color = `rgb(3, 169, 244)`
+                    this.updateCounter(((idx + 1) * 10))
+                } else {
+                    cup.classList.remove(`filled`)
+                    this.mls[idx2].style.color = `black`
+                }
+
+            })
+            this.updateCounterMenos(((idx + 1) * 10))
+        },
+        copao: function () {
+            const fullcups = document.querySelectorAll('.small-cups.filled').length
+            const totalcups = document.querySelectorAll('.small-cups').length
+            const div = (fullcups * 10) / totalcups
+            const div2 = this.range.value * 10 - parseFloat(div * this.range.value).toFixed(1)
+            const div3 = div2 / 10
+            if (fullcups < totalcups) {
+                this.restante.innerHTML = `${div3}L<br>Restantes`
+                this.restante.style.color = `white`
+            } else if (fullcups === totalcups) {
+                this.restante.innerHTML = `Meta diária alcançada!`
+            }
+        },
+        setValue: function () {
+            const newValue = Number((this.range.value - this.range.min) * 100 / (this.range.max - this.range.min))
+            const newPosition = 10 - (newValue * 0.2)
+            this.rangeV.innerHTML = `<span>${this.range.value}</span>`;
+            this.rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+            this.meta.innerText = `Meta Diária: ${this.range.value}L`
+            this.restante.innerHTML = `${this.range.value}L<br>Restantes`
+            this.mls.forEach(ml => {
+                quantidade = this.range.value * 100
+                ml.innerText = `${quantidade}ml`
+                ml.style.color = 'black'
+            })
+            this.smallcups.forEach(cup => {
+                cup.classList.remove('filled')
+            })
+            this.bigwater.style.height = `0%`
+            this.bigwater.nextElementSibling.innerText = '0%'
+        }
     }
-    smallcups.forEach((cup, idx2) => {
-        if (idx2 <= idx) {
-            cup.classList.add(`filled`)
-            bigwater.style.height = `${(idx + 1) * 9.95}%`
-            mls[idx2].style.color = `rgb(3, 169, 244)`
-            updateCounter(((idx + 1) * 10))
-        } else {
-            cup.classList.remove(`filled`)
-            mls[idx2].style.color = `black`
-        }
-
-    })
-    updateCounterMenos(((idx + 1) * 10))
 }
 
-function copao() {
-    const fullcups = document.querySelectorAll('.small-cups.filled').length
-    const totalcups = document.querySelectorAll('.small-cups').length
-    const div = (fullcups * 10) / totalcups
-    const div2 = range.value * 10 - parseFloat(div * range.value).toFixed(1)
-    const div3 = div2 / 10
-    if (fullcups < totalcups) {
-        restante.innerHTML = `${div3}L<br>Restantes`
-        restante.style.color = `white`
-    } else if (fullcups === totalcups) {
-        restante.innerHTML = `Meta diária alcançada!`
-    }
-
-}
-
-// const updateCounter = (numeroAtual, numeroNovo) => {
-//     let n1 = +numeroAtual
-//     let n2 = +numeroNovo
-//     const intervalo = setInterval(()=>{
-
-//         if(n1 <= n2){
-//             bigwater.nextElementSibling.innerText = `${n1++}%`
-//         }else{
-//             clearInterval(intervalo)
-//         }
-//     },20)
-// }
-
-
-const updateCounter = (numeroNovo) => {
-    let n1 = parseInt(bigwater.nextElementSibling.innerText)
-    let n2 = +numeroNovo
-    const intervalo = setInterval(() => {
-
-        if (n1 <= n2) {
-            bigwater.nextElementSibling.innerText = `${n1++}%`
-        } else {
-            clearInterval(intervalo)
-        }
-    }, 20)
-}
-
-const updateCounterMenos = (numeroNovo) => {
-    let n4 = parseInt(bigwater.nextElementSibling.innerText)
-    let n3 = +numeroNovo
-    console.log(n3, n4)
-    const intervalo = setInterval(() => {
-
-        if (n4 >= n3) {
-            bigwater.nextElementSibling.innerText = `${n4--}%`
-        } else {
-            clearInterval(intervalo)
-        }
-    }, 20)
-}
-
-
-const
-    range = document.getElementById('range'),
-    rangeV = document.getElementById('rangeV'),
-    setValue = () => {
-        const
-            newValue = Number((range.value - range.min) * 100 / (range.max - range.min)),
-            newPosition = 10 - (newValue * 0.2);
-        rangeV.innerHTML = `<span>${range.value}</span>`;
-        rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
-        meta.innerText = `Meta Diária: ${range.value}L`
-        restante.innerHTML = `${range.value}L<br>Restantes`
-        mls.forEach(ml => {
-            quantidade = range.value * 100
-            ml.innerText = `${quantidade}ml`
-            ml.style.color = 'black'
-        })
-        smallcups.forEach(cup => {
-            cup.classList.remove('filled')
-        })
-        bigwater.style.height = `0%`
-        bigwater.nextElementSibling.innerText = '0%'
-    };
-document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener('input', setValue);
+Main.init()
